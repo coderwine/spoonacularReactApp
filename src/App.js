d9166aa70f7b8c2f0197d1f72b9240c4a13dca82
Eric <ejwinebrenner@gmail.com>
2021-10-15 20:31:07 -0400

    moved fetch functionality to app.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,89 @@
 // import logo from './logo.svg';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 // Components
 import SiteBar from './components/Navbar/Navbar';
 import SearchRecipe from './components/Recipes/SearchRecipes';
+import spoonKey from './keys';
 
 function App() {
 
-  const appProps = {
-    searchRecipe: 'https://api.spoonacular.com/recipes/complexSearch',
-  }
+  const baseURL = 'https://api.spoonacular.com/recipes/complexSearch'
+
+  const [ inputValue, setInputValue ] = useState('')
+  const [ searchValue, setSearchValue ] = useState('');
+  const [ recipeArr, setRecipeArr ] = useState([]);
+  const [ lastPg, setLastPg ] = useState(0);
+  const [ pgNumber, setPgNumber ] = useState(0); 
+
+  //! SEARCH ITEM
+  useEffect(() => {
+    search()
+  }, [pgNumber, searchValue])
+
+  const searchTerm = () => {
+        setSearchValue(inputValue);
+        setInputValue('');
+    }
+
+    async function search() {
+        console.log(searchValue);
+        console.log(inputValue);
+
+        // let buildURL = `${props.url.searchRecipe}?apiKey=${spoonKey}&query=${searchValue}`;
+        let buildURL = `${baseURL}?apiKey=${spoonKey}&query=${searchValue}`;
+
+        // console.log('pagenumber:', pgNumber);
+
+        if (pgNumber > 0) {
+            buildURL = buildURL + `&offset=${pgNumber}`
+        }
+
+        const response = await fetch(buildURL);
+        const data = await response.json();
+
+        setInputValue('');
+        console.log('data', data);
+        console.log(data.totalResults)
+        setLastPg(data.totalResults);
+        setRecipeArr(data.results);
+        // props.url.recipeArr.push(data.results);
+        // console.log(props);
+    }
+
+    const pageShift = (pos) => {
+        
+        switch (true) {
+            case pos === 'list start':
+                setPgNumber(0);
+                break;
+            case pos === 'back one':
+                setPgNumber(pgNumber-10);
+                break;
+            case pos === 'forward one':
+                setPgNumber(pgNumber+10);
+                break;
+            case pos === 'list end':
+                setPgNumber(lastPg - 10);
+                break;
+            default:
+                console.log('pagination value is invalid');
+        }
+    }
+    
+    //! PROPS
+    const searchProps = {
+      searchRecipe: baseURL,
+      recipeArr: recipeArr,
+      pgShift: () => pageShift(),
+      term: () => searchTerm(),
+      input: setInputValue
+    }
 
   return (
     <div className="App">
       <SiteBar />
-      <SearchRecipe url={appProps} />
+      <SearchRecipe search={searchProps} />
     </div>
   );
 }
--- src/components/Recipes/SearchRecipes.jsx
+++ src/components/Recipes/SearchRecipes.jsx
@@ -1,144 +1,68 @@
-import { React, useState } from 'react';
+import { React, useState, useEffect } from 'react';
 import { Button, Form, FormGroup, Label, Input } from 'reactstrap';
 import { Pagination, PaginationItem, PaginationLink } from 'reactstrap';
 
 import './searchRecipes.css';
-import spoonKey from '../../keys'
+// import spoonKey from '../../keys'
 
 export default function SearchRecipe(props) {
 
-    // console.log(props)
+    console.log(props)
+    const searchProps = props.search;
 
-    const [ searchValue, setSearchValue ] = useState('');
-    const [ recipeDisplay, setRecipeDisplay ] = useState([]);
-    const [ lastPg, setLastPg ] = useState(0);
-
-    // For pagenation
-    let pageNumber = 0;
-
-    async function search(e) {
-        e.preventDefault();
-        console.log(searchValue);
-
-        let buildURL = `${props.url.searchRecipe}?apiKey=${spoonKey}&query=${searchValue}`;
-
-        // console.log('pagenumber:', pgNumber);
-        console.log('pagenumber:', pageNumber);
-
-        // if (pgNumber > 0) {
-        //     buildURL = buildURL + `&offset=${pgNumber}`
-        // }
-        if (pageNumber > 0) {
-            buildURL = buildURL + `&offset=${pageNumber}`
-        }
-
-        console.log(buildURL);
-
-        const response = await fetch(buildURL);
-        const data = await response.json();
-
-        console.log('data', data);
-        console.log(data.totalResults)
-        setLastPg(data.totalResults);
-        setRecipeDisplay(data.results);
-
-    }
-
-    // NOTE: working on pagination.  Offsets by numbers of 10, starting at 0.  Need to hit that last page dynamically and it's not shifting + 10 when indicating.  useState allows it but after a second click.
-
-    const pageShift = (pos, value, event) => {
-        console.log(pos, value);
-        console.log('LAST PAGE:', lastPg)
-        let newPosition = value - 10
-        // console.log(pageNumber, pageNumber + value)
-        switch (true) {
-            case pos === 'list start':
-                // console.log('page number 0');
-                // setPgNumber(0);
-                pageNumber = value;
-                // search();
-                break;
-            case pos === 'back one':
-                // console.log('page number - 10');
-                // setPgNumber(pgNumber-10);
-                pageNumber = (pageNumber - value);
-                // search();
-                break;
-            case pos === 'forward one':
-                // console.log('page number + 10');
-                // setPgNumber(pgNumber+10);
-                pageNumber = newPosition;
-                // search();
-                break;
-            case pos === 'list end':
-                // console.log('page number arr.length');
-                // setPgNumber(100);
-                pageNumber = value;
-                // search();
-                break;
-            default:
-                console.log('pagination value is invalid');
-        }
-
-        search(event);
-
-    }
-
-    // Pageination Build - need to make this so that only portions of it display.
     const paginationDisplay = () => {
         return (
             <Pagination id="pagination-btns" size="md" aria-label="Page navigation">
                 <PaginationItem
-                    onClick={(e) => pageShift('list start', 0, e)} >
-                    <PaginationLink first href="#" />
+                    onClick={(e) => props.search.pgShift('list start')} >
+                    <PaginationLink first />
                 </PaginationItem>
                 <PaginationItem
-                    onClick={(e) => pageShift('back one', 10, e)}>
+                    onClick={(e) => props.search.pgShift('back one')}>
                     <PaginationLink
-                        previous href="#" />
+                        previous />
                 </PaginationItem>
 
                 <PaginationItem
-                    onClick={(e) => pageShift('forward one', 10, e)}>
-                    <PaginationLink next href="#" />
+                    onClick={(e) => props.search.pgShift('forward one')}>
+                    <PaginationLink next />
                 </PaginationItem>
                 <PaginationItem
-                    onClick={(e) => pageShift('list end', lastPg, e)}>
-                    <PaginationLink last href="#" />
+                    onClick={(e) => props.search.pgShift('list end')}>
+                    <PaginationLink last />
                 </PaginationItem>
             </Pagination>
             )
     }
 
-return (
-    <>
-        <div className="search-bar-container">
-            <Form onSubmit={search} className="search-form" inline>
-                <FormGroup>
-                    <Input type="text" name="text" id="search-recipe" placeholder="Input Recipe Here" onChange={(e) => setSearchValue(e.target.value)} />
-                </FormGroup>
-                {' '}
-                <Button id="form-button" color="success">Submit</Button>
-                {paginationDisplay()}
-            </Form>
-        </div>
+    return (
+        <>
+            <div className="search-bar-container">
+                    <Input type="text" name="text" id="search-recipe" placeholder="... what to eat?" onChange={(e) => searchProps.input(e.target.value)} />
+                        {' '}
+                    <Button onClick={props.search.term} id="form-button" color="success">Search</Button>
 
-        <div className="results-container">
-            {
-                recipeDisplay.map((recipe, i) => {
-                    return (
-                        <>
-                            <div className="recipe-item">
-                                <img src={recipe.image} alt={recipe.title} />
-                                <p>{recipe.title}</p>
-                            </div>
-                        </>
-                    )
-                })
-            }
-        </div>
+                    {paginationDisplay()}
+            </div>
+
+            <div className="results-container">
+                {
+                    searchProps.recipeArr.map((recipe, i) => {
+                        return (
+                            <>
+                                <div key={recipe.id} className="recipe-item" id={`${recipe.id}`}>
+                                    <img src={recipe.image} alt={recipe.title} />
+                                    <p>{recipe.title}</p>
+                                </div>
+                            </>
+                        )
+                    })
+                }
+            </div>
 
 
-    </>
-)
+        </>
+    )
 }
+
+// NOTE: Need to adjust  
